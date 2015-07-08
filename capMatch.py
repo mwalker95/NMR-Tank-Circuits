ab21acbf1275d6d6546f4232d2ccc867da0219f9
Morgan <morganwalker@Macintosh.local>
2015-07-08 16:17:53 -0400

    updated capMatch

---
+++ capMatch.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+import math
+
+w = float (input("input frequency in MHz: "))
+z = float (input("input initial impedence: "))
+l = float (input("input inductance in uH: "))
+r = float (input("input internal resistance in Ohm: "))
+w = w*1e6
+l = l*1e-6
+
+
+
+cTune = (l*w*w*z - math.sqrt(r*z*math.pow(l*w*w,2) + math.pow(r,3)*w*w*z - math.pow(r*w*z,2)))/(math.pow(l*w*w,2)*z + z*math.pow(r*w,2))
+
+cMatch = (-1+2*cTune*l*w*w - math.pow(cTune*r*w,2) - math.pow(cTune*l*w*w,2))/(w*w*(-l + cTune*r*r + cTune*math.pow(l*w,2))) 
+
+print("cTune: ", cTune)
+print("cMatch: ", cMatch)
+
