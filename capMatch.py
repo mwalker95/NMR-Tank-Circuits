5ad2c246cb51d9ef89e147e5a54a62dd638d6c16
altoidnerd <altoidnerd@pion.local>
2015-07-08 18:45:49 -0400

    added functions to capMatch and __init__.py

--- capMatch.py
+++ capMatch.py
@@ -1,7 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import sys
 import math
+
+def c_tune(w,l,z,r):
+    c = (l*w*w*z - math.sqrt(r*z*math.pow(l*w*w,2) + math.pow(r,3)*w*w*z - math.pow(r*w*z,2)))/(math.pow(l*w*w,2)*z + z*math.pow(r*w,2))
+    return c
+
+def c_match(w,l,z,r):
+    c = (-1+2*c_tune(w,l,z,r)*l*w*w - math.pow(c_tune(w,l,z,r)*r*w,2) - math.pow(c_tune(w,l,z,r)*l*w*w,2))/    (w*w*(-l + c_tune(w,l,z,r)*r*r + c_tune(w,l,z,r)*math.pow(l*w,2))) 
+    return c
 
 
 def main():
@@ -11,7 +19,9 @@ def main():
     r = float (input("input coil resistance in Ohm: "))
     w = w*1e6
     l = l*1e-6
+
     cTune = (l*w*w*z - math.sqrt(r*z*math.pow(l*w*w,2) + math.pow(r,3)*w*w*z - math.pow(r*w*z,2)))/(math.pow(l*w*w,2)*z + z*math.pow(r*w,2))
+
     cMatch = (-1+2*cTune*l*w*w - math.pow(cTune*r*w,2) - math.pow(cTune*l*w*w,2))/(w*w*(-l + cTune*r*r + cTune*math.pow(l*w,2))) 
 
     print("cTune: ", cTune)
